4f08a09146495f676f6f341fcbbfb34ea38d1261
FaisalZafar781 <faisalzafar781@gmail.com>
2025-01-26 00:00:00 +0500

    chore: update Contents

--- lib/features/shop/screens/product_details/widget/bottom_add_to_cart.dart
+++ lib/features/shop/screens/product_details/widget/bottom_add_to_cart.dart
@@ -91,3 +91,4 @@ class FBottomAddToCart extends StatelessWidget {
 # touched on 2025-05-28T22:55:55.216749Z
 # touched on 2025-05-28T23:00:55.798695Z
 # touched on 2025-05-28T23:02:14.206500Z
+# touched on 2025-05-28T23:05:31.799926Z
