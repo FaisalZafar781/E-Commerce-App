33a89a88d132a6c214f58e84d3ea320f12308c2a
FaisalZafar781 <faisalzafar781@gmail.com>
2025-02-03 00:00:00 +0500

    chore: update payment tile

--- android/app/src/main/kotlin/com/example/af_store/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/af_store/MainActivity.kt
@@ -7,3 +7,4 @@ class MainActivity: FlutterActivity()
 # touched on 2025-05-28T23:02:06.460530Z
 # touched on 2025-05-28T23:03:09.305071Z
 # touched on 2025-05-28T23:05:42.582394Z
+# touched on 2025-05-28T23:06:11.075116Z
